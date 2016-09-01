0a296b9b5efb3729b29c810d596dd3f84857e1c8
Courtney Noonan <courtneynoonan@me.com>
2016-09-01 12:15:30 -0400

    using Faker to seed users, adding user inde and page restrictions

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
+# Use faker to make sample users
+gem 'faker', '1.6.3'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,17 +2,29 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:courtney)
+  end
+
+  test "layout links for logged in users" do
+    log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "no link for users if not logged in" do
+    get root_path
+    assert_template'static_pages/home'
+    assert_select "a[href=?]", users_path, count: 0
   end
 
 end
